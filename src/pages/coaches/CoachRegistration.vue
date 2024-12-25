b0db0f321df20358d7ccb751aac6946b4a3a2cd4
George Luiz 'Maverick' Maluf <georgelmaluf286@gmail.com>
2024-12-25 17:40:43 -0300

    feat: Registering as a coach - form component

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,15 @@
 <template>
-  REGISTER
+  <section>
+    <h2>Register as a coach now!</h2>
+    <coach-form />
+  </section>
 </template>
+
+<script>
+import CoachForm from '@/components/coaches/CoachForm.vue'
+export default {
+  components: {
+    CoachForm
+  }
+}
+</script>
