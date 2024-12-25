65a48e153881378fcbb75315435cbfd9610f4e0b
George Luiz 'Maverick' Maluf <georgelmaluf286@gmail.com>
2024-12-25 15:12:48 -0300

    feat: Working on the coaches list and list items

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,23 +6,45 @@
       <router-link to="/register">Register a Coach</router-link>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="c in filteredCoaches" :key="c.id">
-        {{ c.firstName }}
-      </li>
+      <coach-item
+        v-for="c in filteredCoaches"
+        :id="c.id"
+        :firstName="c.firstName"
+        :lastName="c.lastName"
+        :areas="c.areas"
+        :rate="c.hourlyRate"
+      ></coach-item>
     </ul>
     <h3 v-else>No coaches found.</h3>
   </section>
 </template>
 
 <script>
+import CoachItem from "@/components/coaches/CoachItem.vue";
 export default {
+  components: {
+    CoachItem,
+  },
   computed: {
     filteredCoaches() {
       return this.$store.getters["coaches/coaches"];
     },
     hasCoaches() {
-     return this.$store.getters["coaches/hasCoaches"]
-    }
+      return this.$store.getters["coaches/hasCoaches"];
+    },
   },
 };
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
