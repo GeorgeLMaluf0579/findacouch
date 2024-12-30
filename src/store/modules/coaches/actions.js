48f5a3fd04913159f08a7b683a400065cd10c4af
George Luiz 'Maverick' Maluf <georgelmaluf286@gmail.com>
2024-12-29 21:59:14 -0300

    feat: Fetching coach data (Get http request)

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -12,7 +12,7 @@ export default {
     //Aqui pode ser mudado para axios
 
     const response = await fetch(
-      `https://glm-vue-http-demo-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      `https://glm-vue-http-demo-default-rtdb.firebaseio.com/coaches/${userid}.json`,
       {
         method: "PUT",
         body: JSON.stringify(coachData),
@@ -27,5 +27,32 @@ export default {
       ...coachData,
       id: userId,
     });
+  },
+
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://glm-vue-http-demo-default-rtdb.firebaseio.com/coaches.json`,
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      /// error here
+    }
+
+    const coaches = [];
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+
+    context.commit("setCoaches", coaches);
   },
 };
